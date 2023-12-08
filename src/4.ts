deeb56b97640b4e7d5aea38e2ebd9bc4ca01f24d
SvitlanaSavchenko <134533907+SvitlanaSavchenko@users.noreply.github.com>
2023-12-08 21:34:42 +0100

    homework done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,75 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person entered the house.");
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is now open.");
+    } else {
+      console.log("The key doesn't fit.");
+    }
+  }
+}
+
+// Створення об'єктів
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
+// Перевірка доступу до будинку
 house.openDoor(person.getKey());
 
+// Прибуття людини в будинок
 house.comeIn(person);
-
 
 export {};
